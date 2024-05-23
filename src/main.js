97f9a4cf8e772e8cbd862da5d5288d5b0ff1255f
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-23 23:49:32 +0300

    commit 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
-import './js/header';
-import './js/hero';
-import './js/izitoast';
-import './js/reviews';
-import './js/work-together';
-import './js/covers';
-import './js/accordion';
-import './js/swiper';
+console.log('Hi');

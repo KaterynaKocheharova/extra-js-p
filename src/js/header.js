54e76d038394b62f5e206574ad6a8f536f9e1897
Kateryna Kocheharova <kot41901@gmail.com>
2024-06-02 22:23:10 +0300

    make header fixed onscroll

--- src/js/header.js
+++ src/js/header.js
@@ -2,8 +2,19 @@ import { addClass } from './helpers';
 
 const headerRefs = {
   navLink: document.querySelector('.nav-link'),
+  header: document.querySelector('.header'),
 };
 
 headerRefs.navLink.addEventListener('click', () =>
-  addClass(headerRefs.navLink, 'active')
+  addClass(headerRefs.navLink, 'active-link')
 );
+
+const headerOffset = headerRefs.header.offsetTop;
+
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > headerOffset) {
+    headerRefs.header.classList.add('fixed-header');
+  } else {
+    headerRefs.header.classList.remove('fixed-header');
+  }
+});

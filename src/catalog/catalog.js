f193bee5b32c85683bf461906f122ea89f89b736
Kateryna Kocheharova <kot41901@gmail.com>
2024-06-11 20:14:41 +0300

    style gallery

--- src/catalog/catalog.js
+++ src/catalog/catalog.js
@@ -12,12 +12,14 @@ console.log(productList);
 const productTemplate = ({ id, type, description, colors, gender, price }) => {
   return `<li id=${id} class="product-item">
         <img src="../img/e1084e21-c4e8-4303-a203-72f4838c4cfb.jpg">
-        <h3>${type}</h3>
-        <p>${description}</p>
-        <p>${gender}</p>
-        <p>${price}dollars</p>
-        <a class="product-card-shopping-cart-link" href="cart.html"> <i class="fa-solid fa-bag-shopping"></i></a>
-        <i class="fa-solid fa-heart"></i>
+        <h3 class="catalog-item-title card-title">${type}</h3>
+        <p class="catalog-item-text">${description}</p>
+        <p  class="catalog-item-text catalog-item-price">${price} dollars</p>
+        <div class="catalog-icon-container">
+        <a class="product-card-shopping-cart-link" href="cart.html"> <i class="fa-solid fa-bag-shopping">
+        </i></a>
+        <a class="product-card-fav-link" href="cart.html"><i class="fa-solid fa-heart"></i> </a>
+        </div>
         </li>`;
 };
 
